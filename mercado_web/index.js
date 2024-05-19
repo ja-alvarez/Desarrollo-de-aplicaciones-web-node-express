b236ef9bef78a672a1cbd3551cb3beb1b04f1ccf
Andres Alvarez <juan.alvarez1989@gmail.com>
2024-05-19 03:16:17 -0400

    Cambios index.js, layouts, partials y home

--- mercado_web/index.js
+++ mercado_web/index.js
@@ -1,12 +1,7 @@
-//npx nodemon index.js
-
-const express = require ('express');
+const express = require('express');
 const app = express();
 const { engine } = require('express-handlebars');
-//const exphbs = require('express-handlebars');
 const path = require("path");
-//const { fileURLToPath } = require("url");
-//const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const port = 3000;
 const log = console.log;
@@ -14,16 +9,21 @@ const log = console.log;
 app.use(express.static("public"));
 
 app.listen(port, () => {
-    console.log(`El servidor está inicializado en el puerto ${port}`);
+    log(`El servidor está inicializado en el puerto ${port}`);
 });
 
 app.engine('handlebars', engine({
-	partialsDir: [
-		path.join(__dirname, "/views/partials/")
-	]
+    partialsDir: [
+        path.join(__dirname, "/views/partials/")
+    ],
+    helpers: {
+        // Helper cambiar inicial a mayuscula
+        mayuscula: (str) => {
+            return str.charAt(0).toUpperCase() + str.slice(1);
+        }
+    }
 }));
 
-//app.engine("handlebars", engine); // hbs.engine);
 app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname, "./views"));
 
@@ -31,9 +31,20 @@ app.use('/bootstrap', express.static(path.join(__dirname, "/node_modules/bootstr
 app.use('/jquery', express.static(path.join(__dirname, "/node_modules/jquery/dist")));
 
 app.get(["/", "/home", "/inicio"], (req, res) => {
+    const productos = [
+        { 'nombre': 'platano', 'descripcion': 'Platano Extra Granel 1 kg (3 a 4 un. aprox)', 'precio': '1.390' },
+        { 'nombre': 'cebollas', 'descripcion': 'Cebolla Granel (2 a 4 un. aprox)', 'precio': '1.590' },
+        { 'nombre': 'lechuga', 'descripcion': 'Lechuga escarola un.', 'precio': '1.190' },
+        { 'nombre': 'papas', 'descripcion': 'Papa Granel (4 a 6 un. aprox)', 'precio': '1.590' },
+        { 'nombre': 'pimenton', 'descripcion': 'Pimiento verde un.', 'precio': '990' },
+        { 'nombre': 'tomate', 'descripcion': 'Tomate Larga Vida Granel (2 a 4 un. aprox).', 'precio': '1.990' },
+        { 'nombre': 'manzana', 'descripcion': 'Manzana royal gala granel.', 'precio': '2.190' },
+        { 'nombre': 'naranja', 'descripcion': 'Naranja Importada Granel (4 a 6 un. aprox).', 'precio': '2.690' }
+
+    ];
     res.render("home", {
-        titulo: "Bienvenido al mercado web, seleccione sus productos.",
-        productos: ['banana', 'cebollas', 'lechuga', 'papas', 'pimenton', 'tomate']
+        titulo: "Bienvenido al mercado WEB, seleccione sus productos.",
+        productos: productos
     });
 });
 
